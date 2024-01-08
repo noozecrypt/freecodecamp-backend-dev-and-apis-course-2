cb4828e26b303c99774b143372f0cd19f9f36ab2
noozecrypt <155992322+noozecrypt@users.noreply.github.com>
2024-01-08 18:22:14 +0000

    Task #6
    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,7 +7,11 @@ app.use(express.static(__dirname + '/public'))
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
-app.get("/json", (req, res) => res.send({"message": "Hello json"}));
+app.get("/json", (req, res) => {
+    let body = process.env.MESSAGE_STYLE === 'uppercase' ?
+    {"message": "HELLO JSON"} : {"message": "Hello json"};
+    res.send(body);
+});
 
 
 
