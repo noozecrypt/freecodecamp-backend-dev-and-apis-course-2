f8e50751d225d26066702815e63522f53cd40856
noozecrypt <155992322+noozecrypt@users.noreply.github.com>
2024-01-08 18:09:57 +0000

    Task #5
    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,7 +5,9 @@ console.log("Hello World");
 
 app.use(express.static(__dirname + '/public'))
 app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
+app.get("/json", (req, res) => res.send({"message": "Hello json"}));
 
 
 
