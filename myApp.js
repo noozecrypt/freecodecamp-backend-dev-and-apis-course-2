0f790b0bd0452e1136a23da3b7955ce334994412
noozecrypt <155992322+noozecrypt@users.noreply.github.com>
2024-01-08 17:59:22 +0000

    Task #3
    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => res.send("Hello Express"));
+app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 
 
